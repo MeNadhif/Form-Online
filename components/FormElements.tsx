087193bd492721b0ac53bfaae48eda88c7210334
MeNadhif <nadhif1495@gmail.com>
2024-02-01 11:35:17 +0700

    Number and TextArea field on the sidebar menu

--- components/FormElements.tsx
+++ components/FormElements.tsx
@@ -1,7 +1,9 @@
+import { NumberFieldFormElement } from "./fields/NumberField";
 import { ParagraphFieldFormElement } from "./fields/ParagraphField";
 import { SeparatorFieldFormElement } from "./fields/SeparatorField";
 import { SpacerFieldFormElement } from "./fields/SpacerField";
 import { SubtitleFieldFormElement } from "./fields/SubTitleField";
+import { TextAreaFormElement } from "./fields/TextAreaField";
 import { TextFieldFormElement } from "./fields/TextField";
 import { TitleFieldFormElement } from "./fields/TitleField";
 
@@ -11,7 +13,9 @@ export type ElementsType =
    | "SubTitleField"
    | "ParagraphField"
    | "SeparatorField"
-   | "SpacerField";
+   | "SpacerField"
+   | "NumberField"
+   | "TextAreaField";
 
 export type SubmitFunction = (key: string, value: string) => void;
 
@@ -56,4 +60,6 @@ export const FormElements: FormElementsType = {
    ParagraphField: ParagraphFieldFormElement,
    SeparatorField: SeparatorFieldFormElement,
    SpacerField: SpacerFieldFormElement,
+   NumberField: NumberFieldFormElement,
+   TextAreaField: TextAreaFormElement,
 };
--- components/FormElementsSidebar.tsx
+++ components/FormElementsSidebar.tsx
@@ -21,6 +21,8 @@ function FormElementsSidebar() {
                Form elements
             </p>
             <SidebarBtnElement formElement={FormElements.TextField} />
+            <SidebarBtnElement formElement={FormElements.NumberField} />
+            <SidebarBtnElement formElement={FormElements.TextAreaField} />
          </div>
       </div>
    );
